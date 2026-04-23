a347cd0d5d244a18adea1f58dd75d5c2c656a152
Nikhil-Jha-03 <jhanikhil2083@gmail.com>
2026-04-23 23:15:34 +0530

    Added

--- Basic_Math/Main.java
+++ Basic_Math/Main.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
     static void extractNumber() {
@@ -117,7 +121,7 @@ public class Main {
         int x = 9474;
         int num = x;
         int result = 0;
-        // int count = String.valueOf(x).length();  // can also do something like this 
+        // int count = String.valueOf(x).length(); // can also do something like this
 
         int count = 0;
         while (num != 0) {
@@ -140,12 +144,46 @@ public class Main {
         }
     }
 
+    static void printAllDivisorsOfGivenNumber() {
+
+        // initally if we loop till num time which has time complexity of O(n),
+        // So if we have 10000 , the loop run for that much time
+        // Therefore we use square root time to run the loop
+        // So what happens here is loop goes for sqrt(num) --> 36 -->6
+        // 1st it check if num % i == 0 then in the same iteration it is check that if
+        // the num / i != i
+        // if not then the number is added generally it is from the back eg. num / i(1)
+        // = 36 and push it to the array
+
+        // current time complexity is O(squrt(n))
+        int num = 36;
+        List<Integer> arr = new ArrayList<Integer>();
+
+        for (int i = 1; i <= Math.sqrt(num); i++) {
+            System.out.println(i);
+            if (num % i == 0) {
+                // arr.add(i);
+                if (num / i != i) {
+                    System.out.println(num / i);
+                    arr.add(num / i);
+                }
+            }
+        }
+
+        Collections.sort(arr);
+
+        System.out.println(arr);
+
+    }
+
+    static void checkForPrimeNumber() {}
+
     public static void main(String[] args) {
         // extractNumber(); extract and remove number
         // countAllDigitsOfANumber(); count the lenght of the number
         // reverseNumber();
         // checkNumberPalindrome();
-        isArmstrongNumber();
-
+        // isArmstrongNumber();
+        printAllDivisorsOfGivenNumber();
     }
 }
