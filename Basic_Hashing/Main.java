be59a6c67e2da59c2d1203074d9be3c356bb9a95
Nikhil-Jha-03 <jhanikhil2083@gmail.com>
2026-05-10 15:30:24 +0530

    basic hashing over

--- Basic_Hashing/Main.java
+++ Basic_Hashing/Main.java
@@ -1,11 +1,8 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
 
+    static void hashing1() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Array Length");
         int num = sc.nextInt();
@@ -24,16 +21,101 @@ public class Main {
             if (helo == null) {
                 hm.put(arr[i], 1);
             } else {
-                hm.put(arr[i], hm.getOrDefault(arr[i], 0)+1);
+                hm.put(arr[i], hm.getOrDefault(arr[i], 0) + 1);
             }
-
         }
 
         System.out.println("HM: " + hm);
 
-        // HashMap<Integer , Integer> hm = new HashMap<>();
-        // List<Integer> data = new ArrayList<{2,3,4,5,2,4,6}>;
-
         sc.close();
     }
+
+    static void characterHashing2() {
+        String str = "abcdefhfdgh";
+        Map<Character, Integer> map = new HashMap<>();
+
+        // for (int i = 0; i < str.length(); i++) {
+        // if (map.containsKey(str.charAt(i))) {
+        // map.put(str.charAt(i), map.get(str.charAt(i)) + 1);
+        // } else {
+        // map.put(str.charAt(i), 1);
+        // }
+        // }
+
+        for (int i = 0; i < str.length(); i++) {
+            Integer helo = map.get(str.charAt(i));
+
+            if (helo == null) {
+                map.put(str.charAt(i), 1);
+            } else {
+                map.put(null, map.getOrDefault(str.charAt(i), 0) + 1);
+            }
+        }
+
+        System.out.println(map);
+    }
+
+    static void hashing3() {
+        int arr[] = { 2, 2, 3, 4, 4, 2 };
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (map.containsKey(arr[i])) {
+
+                map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+            } else {
+                map.put(arr[i], 1);
+            }
+        }
+
+        for (Map.Entry<Integer, Integer> mp : map.entrySet()) {
+            System.out.println(mp.getKey() + " -> " + mp.getValue());
+        }
+
+        // System.out.println(map);
+
+    }
+
+    static void highestlowestFrequencyElement() {
+        int arr[] = { 10, 5, 10, 15, 10, 5 };
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (map.containsKey(arr[i])) {
+
+                map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+            } else {
+                map.put(arr[i], 1);
+            }
+        }
+
+        int maxElement = 0, maxFrequency = 0;
+        int minElement = 0, minFrequency = arr.length;
+
+        for (Map.Entry<Integer, Integer> mp : map.entrySet()) {
+            int element = mp.getKey();
+            int count = mp.getValue();
+
+            if (count < minFrequency) {
+                minFrequency = count;
+                minElement = element;
+            }
+
+            if (count > maxFrequency) {
+                maxFrequency = count;
+                maxElement = element;
+            }
+        }
+
+        System.out.println(maxElement + " " + minElement);
+
+    }
+
+    public static void main(String[] args) {
+        // hashing1();
+        // hashing3();
+        highestlowestFrequencyElement();
+    }
+
 }
