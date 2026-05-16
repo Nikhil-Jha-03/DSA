163c44ef7b9da41af7ea77d40f10b7dd95ab33b6
Nikhil-Jha-03 <jhanikhil2083@gmail.com>
2026-05-16 21:47:24 +0530

    Quick Sort

--- Sorting/Main.java
+++ Sorting/Main.java
@@ -112,12 +112,6 @@ public class Main {
             arr[i] = temp.get(i - low);
         }
 
-
-        for(int a : arr){
-            System.out.print( a + " ");
-        }
-        
-
     }
 
     static void mergeSort(int[] arr, int low, int high) {
@@ -127,21 +121,122 @@ public class Main {
         }
 
         int mid = (low + high) / 2;
-        
+        // Left Divided Array
         mergeSort(arr, low, mid);
+        // Right Divided Array
         mergeSort(arr, mid + 1, high);
+        // For Sorting
         mergeArray(arr, low, mid, high);
     }
 
+    static void recursiveBubbleSort(int arr[], int n, int left) {
+        System.out.println("N: " + n);
+        if (n == 1) {
+            return;
+        }
+
+        if (left == n) {
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > arr[i + 1]) {
+                int temp = arr[i + 1];
+                arr[i + 1] = arr[i];
+                arr[i] = temp;
+            }
+        }
+
+        recursiveBubbleSort(arr, n - 1, left);
+
+    }
+
+    static void recursiveInsertionSort(int[] arr, int n, int start) {
+        if (start > n) {
+            return;
+        }
+        int j = start;
+
+        while (j > 0 && arr[j] < arr[j - 1]) {
+            int temp = arr[j - 1];
+            arr[j - 1] = arr[j];
+            arr[j] = temp;
+            j--;
+        }
+
+        recursiveInsertionSort(arr, n, start + 1);
+    }
+
+    static int findPartition(int[] arr, int left, int right) {
+        // { 4,6,2,5,7,9,1,3};
+
+        int pivot = arr[left];
+        int i = left;
+        int j = right;
+
+        while (i < j) {
+
+            while (arr[i] <= pivot && i <= right - 1) {
+                i++;
+            }
+
+            while (arr[j] > pivot && j >= left + 1) {
+                j--;
+            }
+
+            if (i < j) {
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+
+        }
+
+        int temp = arr[left];
+        arr[left] = arr[j];
+        arr[j] = temp;
+
+        return j;
+    }
+
+    static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int partition = findPartition(arr, low, high);
+            quickSort(arr, low, partition - 1);
+            quickSort(arr, partition + 1, high);
+        }
+
+    }
+
     public static void main(String[] args) {
+        
         // int arr[] = { 13, 46, 24, 52, 20, 9 };
         // int arr[] = { 9, 14, 15, 12, 6, 8, 13 };
         // int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
         // selectionSort(arr);
         // bubbleSort(arr);
         // insertionSort(arr);
-        int arr[] = { 3, 1, 2, 4, 1, 5, 2, 6, 4 };
-        mergeSort(arr, 0, arr.length - 1);
-    }
+        // int arr[] = { 3, 1, 2, 4, 1, 5, 2, 6, 4 };
+        // mergeSort(arr, 0, arr.length - 1);
+        // for(int a : arr){
+        // System.out.print( a + " ");
+        // }
 
+        // recursiveBubbleSort(arr, arr.length - 1, 1);
+        // for (int a : arr) {
+        // System.out.print(a + " ");
+        // }
+
+        // recursiveInsertionSort(arr, arr.length - 1, 1);
+        // for (int a : arr) {
+        // System.out.print(a + " ");
+        // }
+
+        int arr[] = { 4, 6, 2, 5, 7, 9, 1, 3 };
+        quickSort(arr, 0, arr.length - 1);
+        for (int a : arr) {
+            System.out.print(a + " ");
+        }
+
+    }
 }
