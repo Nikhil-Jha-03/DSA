449bdf460e96e1f82d2ac3e4ac0c9c2167008244
Nikhil-Jha-03 <jhanikhil2083@gmail.com>
2026-05-25 00:20:53 +0530

    Added

--- Arrays/Easy/Main.java
+++ Arrays/Easy/Main.java
@@ -63,9 +63,93 @@ public class Main {
 
     }
 
+    static void secondSmallestElementInAnArray() {
+        int arr[] = { 1, 2, 4, 7, 7, 5, 7, 7, 7, 8, 8, 8, 3, 4, 5, 7, 8, 9, 13, 14 };
+        // brute = sorting O(n log n) -> select arr[1]; as the second smallest -- also
+        // check size of the array ie it has to have atleast two element
+
+        // better apporach O(2n)
+
+        int smallest = arr[0];
+        int secondSmallest = Integer.MAX_VALUE;
+
+        // for (int i = 0; i < arr.length; i++) {
+        // if (arr[i] < smallest) {
+        // smallest = arr[i];
+        // }
+        // }
+
+        // smallest = 1;
+        // for (int i = 0; i < arr.length; i++) {
+        // if (arr[i] > smallest && arr[i] < secondSmallest) {
+        // secondSmallest = arr[i];
+        // }
+        // }
+
+        // optimal approach
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < smallest) {
+                secondSmallest = smallest;
+                smallest = arr[i];
+            } else if (arr[i] > smallest && arr[i] < secondSmallest) {
+                secondSmallest = arr[i];
+            }
+        }
+        System.out.println(secondSmallest);
+
+    }
+
+    static boolean checkIfAnArrayIsSorted() {
+        int[] arr = { 1, 2, 3, 4, 5, 2 };
+
+        // brute force approach;
+        // O(n^2)
+        // for (int i = 0; i < arr.length; i++) {
+        // for (int j = i + 1; j < arr.length; j++) {
+        // if (arr[j] < arr[i]) {
+        // return false;
+        // }
+        // }
+        // }
+
+        // Optimal approach;
+        // O(n)
+        for (int i = 0; i < arr.length - 1; i++) { // no index out of bound error so lenght -1
+            // if (arr[i - 1] < arr[i] && arr[i] < arr[i + 1]) {
+            // return true;
+            // }
+
+            if (arr[i] > arr[i + 1]) {
+                System.out.println("unsorter");
+                return false;
+            }
+        }
+        System.out.println("Sorted");
+        return true;
+    }
+
+    static boolean checkIfArrayIsSortedAndRotated() {
+        int[] arr = { 3, 4, 5, 1, 2 };
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >  arr[(i+1)%arr.length]) {
+                count++;
+                if (count > 1) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+
+
+
+    }
+
     public static void main(String[] args) {
         // largestElementInAnArray();
-        secondLargestElementInAnArray();
-
+        // secondLargestElementInAnArray();
+        // secondSmallestElementInAnArray();
+        checkIfAnArrayIsSorted();
     }
 }
